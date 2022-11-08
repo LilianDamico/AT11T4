53adb77dc58b3b31350ddd920b604413b57609bb
lfons <lfons@DESKTOP-8PJVQIC>
2022-11-08 09:15:44 -0300

    Adicionar arquivos de projeto.

---
+++ AT11T4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Inicio
+int id = 0;
+int cont;
+int p = 1;
+cont = 0;
+while (cont <= 10)
+{
+    Console.WriteLine("Informe a idade: ");
+    id = Convert.ToInt32(Console.ReadLine());
+   
+    if (id > 18)
+    {
+        p++;
+    }
+    cont++;
+}id++;
+Console.WriteLine("Foram inseridas " + p + " pessoas maiores de idade.");
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
+//Fim
